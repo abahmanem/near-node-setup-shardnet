bedfc55b7f9efb25813d8a269a61fb6800512157
abahmanem <101295246+abahmanem@users.noreply.github.com>
2022-07-22 16:23:42 +0200

    Update loadDatas.js

--- challenge-7/loadDatas.js
+++ challenge-7/loadDatas.js
@@ -1,1 +1,82 @@
 
+```node
+const request = require('request');
+const { Pool, Client } = require("pg");
+const pool = new Pool({
+  user: "superset",
+  host: "xxx.xxx.xxx.xxx",
+  database: "stakewars_iii",
+  password: "xxxxxxxxx",
+  port: 5432,
+});
+
+//RPC Call
+let options = {
+    url: "http://xxx.xxx.xxx.xxx:3030",
+    method: "post",
+    headers:
+    {
+     "content-type": "Application/json"
+    },
+
+    body: JSON.stringify( {"jsonrpc": "2.0", "id": "dontcare",  "method": "validators",  "params": [null]})
+};
+
+
+request(options, (error, response, body) => {
+    if (error) {
+        console.error('An error has occurred: ', error);
+    } else {
+           var data = JSON.parse(body);
+           var current_validators= data['result']['current_validators'];
+
+                                                        const inserts = [
+                                            "account_id",
+                                            "public_key",
+                                            "is_slashed",
+                                            "num_expected_blocks",
+                                            "num_produced_blocks",
+                                            "num_expected_chunks",
+                                            "num_produced_chunks",
+                                            "shards",
+                                            "stake",
+                                            "rpc_timestamp"
+                                        ];
+
+                                        current_validators.forEach(function(e)
+                                        {
+                                            console.log('inserting new data for ', e['account_id']);
+
+																				let date_ob = new Date();
+																				// current date
+																				let date = ("0" + date_ob.getDate()).slice(-2);
+																				// current month
+																				let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+																				// current year
+																				let year = date_ob.getFullYear();
+																				
+																				// current hours
+																				let hours = date_ob.getHours();
+																				
+																				// current minutes
+																				let minutes = date_ob.getMinutes();
+																				
+																				// current seconds
+																				let seconds = date_ob.getSeconds();
+																				
+																				var ts = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+																				var values = "'"+e['account_id']+"','"+e['public_key']+"','"+e['is_slashed']+"','"+e['num_expected_blocks']+"','"+e['num_produced_blocks']+"','"+e['num_expected_chunks']+"','"+e['num_produced_chunks']+"','"+e['shards']+"','"+e['stake']+"','"+ts+"'";
+																				                                           console.log('values  ', values);
+																				                                           let sqlString = `INSERT INTO current_validators(${inserts}) VALUES(${values})`;
+																				                                          pool.query(sqlString,(err, res) => {
+																				                                                console.log(err, res);
+																				                                               // pool.end();
+																				                                                }
+																				                                          );
+																				
+																				                                         });
+																				     }
+																				
+});
+//pool.end();
+
